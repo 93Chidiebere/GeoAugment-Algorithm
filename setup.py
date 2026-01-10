b7835b5e1bad6fb8b085af779c606c48c5686912
93chidiebere <vincent.christopher.189736@unn.edu.ng>
2026-01-10 12:15:50 +0100

    Updated GeoAugment version for upload

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="geoaugment",
-    version="0.1.3",
+    version="0.1.4",
     description="Constraint-aware synthetic geospatial data augmentation engine for GeoAI",
     long_description=long_description,
     long_description_content_type="text/markdown",
